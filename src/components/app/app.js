d62f0690d690e3d68bbeaaef249e1ef25fa8d4a4
zed <zed@mail.ru>
2019-11-22 00:45:44 +0300

    refactoring

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,33 +1,57 @@
 import React from "react";
 import Header from "../header/header";
-import RandomPlanet from "../random-planet/random-planet";
-import PersonsPage from "../persons-page/person-page";
-import Planetdetails from "../planet-details/planet-details";
-import StarshipDetails from "../starship-details/starship-details";
+//import RandomPlanet from "../random-planet/random-planet";
+import {ItemDetails, Record} from "../item-details/item-details";
+//import ItemPage from "../item-page/item-page";
+import ErrorBoundry from "../error-boundry/error-boundry";
+import Row from "../row/row";
 import "./app.css";
 import Swapi from "../../services/swapi";
 
 class App extends React.Component{
     swapi = new Swapi();
     state = {        
-        showRandomPlanet:true
+        showRandomPlanet:true,
     }
     
     render(){
+        const {
+            getPerson, 
+            getStarship, 
+            getStarshipImage,
+            getPersonImage
+        } = this.swapi;
+        const personDetails = (
+            <ItemDetails 
+                id={10} 
+                getData={getPerson} 
+                getImgUrl={getPersonImage}>
+                <Record field="gender" label="Gender"/>
+                <Record field="eyeColor" label="Eye Color"/>
+            </ItemDetails>
+        );
+        
+        const starshipDetails = (
+            <ItemDetails 
+                id={10} 
+                getData={getStarship} 
+                getImgUrl={getStarshipImage}
+            >
+                <Record field="model" label="Model"/>
+                <Record field="length" label="Length"/>
+                <Record field="costInCredits" label="Cost"/>                
+            </ItemDetails>
+        );
+
         return (
-            <div className="container">
-                <div className="row w-100" >
+            <ErrorBoundry>
+                <div className="container">
                     <Header/>
+                    <Row
+                        left={personDetails}
+                        right={starshipDetails}/>
                 </div>
-                <div className="row">
-                <RandomPlanet/>
-                </div>
-                
-                <PersonsPage swapi={this.swapi.getAllPeople}/>
-                <PersonsPage swapi={this.swapi.getAllPlanets}/>
-                <PersonsPage swapi={this.swapi.getAllStarships}/>
-                
-            </div>
+            </ErrorBoundry>            
         );
     }
 }
