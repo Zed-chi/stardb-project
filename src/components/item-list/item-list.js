0179b59100ff7508dd3f0f4d299dec8ddf77a10b
Zed-chi <zed-chi@mail.ru>
2019-11-25 15:39:18 +0300

    moved logic to new file

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,65 +1,33 @@
 import React from "react";
 import "./item-list.css";
 import Spinner from "../spinner/spinner";
+import Swapi from "../../services/swapi";
+import withData from "../hoc-helpers/with-data";
 
 
-class ItemList extends React.Component{
-
-    renderItems = (itemList)=> {
-        return itemList.map((item)=>{
-            const {id} = item;
-            const name = this.props.renderItem(item);
-            return (<li 
-                    key={id}
-                    onClick={()=> this.props.onItemSelected(id)}
-                    className="list-group-item list-group-item-action"
-                >
-                    {name}
-                </li>);
-            }
-        );  
-    };
-
-    render(){
-        const {data} = this.props;
-        const content = this.renderItems(data);
-        return (
-            <div className="col-md-6 card p-2">
-                <ul className="list-group">
-                   {content} 
-                </ul>
-            </div>
-        );
-    }
+const ItemList = (props) => {
+    const {data, onItemSelected, children: renderItem} = props;
+    const items = data.map((item)=>{
+        const {id} = item;
+        const name = renderItem(item);
+        return (<li 
+                key={id}
+                onClick={()=> onItemSelected(id)}
+                className="list-group-item list-group-item-action"
+            >
+                {name}
+            </li>);
+        }
+    );
+    return (
+        <div className="col-md-6 card p-2">
+            <ul className="list-group">
+                {items} 
+            </ul>
+        </div>
+    );
 };
 
 
-const withData = (View, getData)=>{
-    return class extends Component{
-            
-        state = {
-            data:[]
-        }
-
-        componentDidMount(){
-            const {getData} = this.props;
-            getData()
-            .then((data)=>{
-                this.setState({
-                    data
-                })
-            })
-        }
-
-        render(){
-            const {data} = this.state;
-            if (!data){
-                return <Spinner/>;
-            }
-            return <View {...this.props} data={data}/>;
-        }
-    };
-};
-
-
-export default withData(ItemList);
+const {getAllPeople} = new Swapi();
+export default withData(ItemList,getAllPeople);
