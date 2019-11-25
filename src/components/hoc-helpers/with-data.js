2e70138b12f55356d5f92e3f2aeb6fb1a81fb5e8
Zed-chi <zed-chi@mail.ru>
2019-11-25 15:39:35 +0300

    created new hoc

---
+++ src/components/hoc-helpers/with-data.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Spinner from "../spinner/spinner";
+import ErrorBoundry from "../error-boundry/error-boundry";
+
+
+const withData = (View, getData)=>{
+    return class extends React.Component{
+            
+        state = {
+            data:[]
+        }
+
+        componentDidMount(){
+            getData()
+            .then((data)=>{
+                this.setState({
+                    data
+                })
+            })
+        }
+
+        render(){
+            const {data} = this.state;
+            if (!data){
+                return <Spinner/>;
+            }
+            return <ErrorBoundry>
+                <View {...this.props} data={data}/>;
+            </ErrorBoundry>
+            
+        }
+    };
+};
+
+export default withData;
